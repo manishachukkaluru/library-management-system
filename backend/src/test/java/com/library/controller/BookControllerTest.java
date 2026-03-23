e573b839bb463116b7e952129a5e21648c8031ee
manishachukkaluru <119605931+manishachukkaluru@users.noreply.github.com>
2026-03-23 15:29:20 +0530

    fixing ci/cd failures for banckend

--- backend/src/test/java/com/library/controller/BookControllerTest.java
+++ backend/src/test/java/com/library/controller/BookControllerTest.java
@@ -9,6 +9,7 @@ import com.library.service.BookService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
+@AutoConfigureDataJpa
 @DisplayName("BookController Integration Tests")
 class BookControllerTest {
 
